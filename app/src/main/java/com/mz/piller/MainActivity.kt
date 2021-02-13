94e3129db7713ccc2b802706703b443fff1ff18d
Malik Ziq <45529953+malikziq@users.noreply.github.com>
2021-02-13 13:46:37 +0200

    Initial Project

---
+++ app/src/main/java/com/mz/piller/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mz.piller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
